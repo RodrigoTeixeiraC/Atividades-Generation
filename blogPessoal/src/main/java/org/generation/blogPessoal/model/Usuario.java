6948bf93f588f4a744508ac0c8f89fe918f85a3d
RodrigoTeixeiraC <rodrigo.tchagas@gmail.com>
2021-05-08 08:25:40 -0300

    Security

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,67 @@
+package org.generation.blogPessoal.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Size(min = 2, max = 100)
+	@NotBlank(message = "O campo nome não pode ser vazio!")
+	private String nome;
+
+	@Column(unique = true)
+	@Size(min = 2, max = 100)
+	@NotBlank(message = "O campo E-mail não pode ser vazio!")
+	private String usuario;
+
+	@Column(unique = true)
+	@Size(min = 8, max = 25)
+	@NotBlank(message = "O campo CPF não pode ser vazio!")
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
